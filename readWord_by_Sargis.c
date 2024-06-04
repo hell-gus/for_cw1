4bb5e56faf93cd440e945f732b7fa7f76dc76669
hell-gus <144446200+hell-gus@users.noreply.github.com>
2024-06-05 02:25:35 +0300

    Create readWord_by_Sargis.c

---
+++ readWord_by_Sargis.c
@@ -0,0 +1,34 @@
+Word readWord(){
+    int size = MEMORY_STEP;
+    int len = 0;//длина слова
+    wchar_t *chars = malloc(size * sizeof(wchar_t));// массив символов без разделителей
+    wchar_t sep, symbol = L' ';// символ разделитель 
+
+    while(symbol == L' ')
+        symbol = getwchar();
+
+    while((!wcschr(WORD_SEP, symbol)) && (!wcschr(SENTENCE_SEP, symbol))){
+
+        if (symbol==L'\n') newlineCounter++;
+        else newlineCounter=0;
+        if (newlineCounter==2) break;
+  
+ 
+        chars[len++] = symbol;
+  
+        
+        if(len == size){
+            size += MEMORY_STEP;
+            chars = (wchar_t*)realloc(chars, size * sizeof(wchar_t));
+        }
+     
+        symbol = getwchar();
+        
+    }
+    
+    chars[len] = L'\0';
+    
+    sep = symbol;
+  
+    return (Word){chars, sep, len};
+}
